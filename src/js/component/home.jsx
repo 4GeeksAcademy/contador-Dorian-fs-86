07e5a1ab80b3d3c25d5c586ef0f8c530568b7f3c
Dorian Zuluaga <166706826+Dorianzuluaga@users.noreply.github.com>
2024-12-11 00:10:14 +0000

    contador Dorian

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,88 +1,41 @@
 import React, { useState, useEffect,} from "react";
 
 
-//include images into your bundle
-// import rigoImage from "../../img/rigo-baby.jpg";
+function App () {
+const [seconds, setSeconds]=useState(0);
 
-//create your first component
 
-// contador de segundos
+useEffect(() => {
+    const interval = setInterval(() => {
+        setSeconds((prev) => prev + 1);
+    }, 1000);
 
-const SecondsCounter = ({seconds}) => {
-	return (
-		<div className="seconds-counter">
-			<h1>
-			<i class="fa-solid fa-clock"></i> {seconds}
-			</h1>
-		</div>
-	);
+    return () => clearInterval(interval);
+}, []);
+
+useEffect(() => {
+    if(seconds === 10){
+        alert("Has alcanzado 10 segundos CONGRATS! ");
+    }
+}, [seconds]);
+
+const minutes = Math.floor((seconds % 3600) / 60);
+const countseconds = seconds % 60;
+
+return(
+    <div>
+    <div className="image">
+    <h1 className="titulo">ANALOG WATCH TIMER</h1>
+    <div className="padrecontador">
+    <div>
+    <div>
+    <span className="segundos">{String(countseconds).padStart(2, "0")} segundos </span>
+    </div>
+    <span className="minutos">{String(minutes).padStart(2, "0")} minutos </span>
+    </div>
+    </div>
+    </div>
+    </div>
+);
 };
-
-const Home = () => {
-	const [seconds, setSeconds] = useState(0);
-	const [isRunning, setIsRunning] = useState(true);
-	const [countdownValue, setCountdownValue] = useState("");
-
-
-	// para manejar el intérvalo
-	useEffect(() => {
-		let interval;
-		if (isRunning) {
-			interval = setInterval(() => {
-				setSeconds((prevSeconds) => {
-					if (prevSeconds > 0) {
-						return prevSeconds - 1;
-					} else {
-						setIsRunning(false);
-						return 0;
-					}
-				});
-		}, 1000);
-		}
-
-
-	// para limpiar el intérvalo
-		return () => clearInterval(interval);
-	}, [isRunning]);
-
-
-	//función para detener el intérvalo 
-	const stopCounter = () => 
-		setIsRunning(false);
-
-
-	//función para resetear el intérvalo 
-	const resetCounter = () => {
-		setSeconds(0);
-		setIsRunning(true);
-	};
-
-	// función para la cuenta regresiva
-	const startCountdown = () => {
-		if (countdownValue && parseInt(countdownValue) > 0){
-			setSeconds(parseInt(countdownValue)); 
-			setIsRunning(true);
-		}
-	};
-	
-	return ( 
-		<div className="imagen">
-		<h1>Que comience la fiesta</h1>
-			<SecondsCounter seconds={seconds}/>
-			<div>
-				<button onClick={stopCounter}>Stop</button>
-				<button onClick={resetCounter}>Reset</button>
-			</div>
-			<div>
-				<input
-				type="number"
-				placeholder="Cuenta regresiva"
-				value={countdownValue}
-				onChange={(e) => setCountdownValue(e.target.value)}
-				/>
-				<button onClick={startCountdown}>para inciar la cuenta regresiva</button>
-			</div>
-		</div>
-	);};
-
-export default Home;
+export default App
